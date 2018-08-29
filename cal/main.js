e62327de8811112fda273e6034fd10801ae18189
Giovanni <giorereyes@gmail.com>
2018-08-28 20:05:18 -0500

    Added cal.

---
+++ cal/main.js
@@ -0,0 +1,38 @@
+function addNumber(num) {
+    document.querySelector("#results").value += num;
+  }
+  
+  function clearResults() {
+    document.querySelector("#results").value = "";
+  }
+  
+  function addition() {
+    document.querySelector("#results").value += "+";
+  }
+  
+  function subtraction() {
+    document.querySelector("#results").value += "-";
+  }
+  
+  function multi() {
+    document.querySelector("#results").value += "*";
+  }
+  
+  function inverse() {
+    document.querySelector("#results").value = 
+    eval(document.querySelector("#results").value)*-1;
+  }
+  
+  function divide() {
+    document.querySelector("#results").value += "/";
+  }
+  
+  
+  function equals() {
+    document.querySelector("#results").value = eval(document.querySelector("#results").value);
+  }
+  
+  function deleteLast() {
+    let current = document.querySelector("#results").value;
+    document.querySelector("#results").value = current.slice(0, -1);
+  }
